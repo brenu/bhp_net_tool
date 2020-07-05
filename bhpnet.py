6477f06da1f68500fb20ac842e5c9cea359736a0
brenu <bvsousa64@gmail.com>
2020-07-05 15:31:34 -0400

    :construction: Import libraries and define global variables

--- bhpnet.py
+++ bhpnet.py
@@ -1,1 +1,14 @@
-print('Hello World!')
+import sys
+import socket
+import getopt
+import threading
+import subprocess
+
+# Defining global variables
+listen = false
+command = false
+upload = false
+execute = ""
+target = ""
+upload_destination = ""
+port = 0
