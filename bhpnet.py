c2f2249e97bc970af791e232bd4fcea213d5519b
brenu <bvsousa64@gmail.com>
2020-07-05 16:51:50 -0300

    :construction: Implement usage banner function

--- bhpnet.py
+++ bhpnet.py
@@ -5,10 +5,29 @@ import threading
 import subprocess
 
 # Defining global variables
-listen = false
-command = false
-upload = false
+listen = False
+command = False
+upload = False
 execute = ""
 target = ""
 upload_destination = ""
 port = 0
+
+def usage():
+    print("BHP Net Tool")
+    print()
+    print("Usage: bhpnet.py -t target_host -p port")
+    print("-l --listen               - listen on [host]:[port] for incoming connections")
+    print("-e --execute=file_to_run  - execute the given file upon receiving a connection")
+    print("-c --comand               - initialize a command shell")
+    print("-u --upload=destination   - upon receiving connection upload a file and write to [destination]")
+    print()
+    print()
+    print("Examples: ")
+    print("bhpnet.py -t 192.168.0.1 -p 5555 -l -c")
+    print("bhpnet.py -t 192.168.0.1 -p 5555 -l -u=C:\\target.exe")
+    print("bhpnet.py -t 192.168.0.1 -p 5555 -l -e=\"cat /etc/passwd\"")
+    print("echo 'ABCDEFGHI' | ./bhpnet.py -t 192.168.0.1 -t 192.168.11.12 -p 135")
+    sys.exit(0)
+
+usage()
